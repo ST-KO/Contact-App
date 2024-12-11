38debc5580f3d5509cc3a87d9d32d374d5f0d194
ST-Ko <sithu.ko21120@gmail.com>
2024-12-12 01:15:59 +1100

    deploy via github

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "http://ST-KO.github.io/Contact-App",
+  base: "/Contact-App/",
   plugins: [react()],
   test: {
     globals: true,
